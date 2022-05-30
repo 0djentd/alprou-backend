cc1c2824e6c185d083f21be4883e9dd119c8341e
0djentd <58089540+0djentd@users.noreply.github.com>
2022-05-31 00:28:53 +0300

    modified:   core/models.py

--- core/models.py
+++ core/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, auto_created=True)
+
+
+class Day(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+
+
 class Habit(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=200)
     negative = models.BooleanField(default=False)
-    days_completed = ArrayField(models.DateTimeField(blank=False, null=False), blank=True)
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, auto_created=True)
