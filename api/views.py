f50c7bc6391f1949201a83ddd3fc363eee9b6886
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-13 17:12:51 +0300

    Bugfix. Added views.

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.permissions import IsAdminUser
 
 from core.models import Profile
@@ -7,7 +7,15 @@ from . import serializers
 from . mixins import IsObjectAuthorPermission
 
 
-class ProfilesListAPIView(IsObjectAuthorPermission, generics.ListAPIView):
+class ProfilesListCreateAPIView(IsObjectAuthorPermission,
+                                generics.ListCreateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+    permission_classes = [IsObjectAuthorPermission, IsAdminUser]
+
+
+class ProfileDetailsAPIView(IsObjectAuthorPermission,
+                            generics.RetrieveUpdateDestroyAPIView):
     queryset = Profile.objects.all()
     serializer_class = serializers.ProfileSerializer
     permission_classes = [IsObjectAuthorPermission, IsAdminUser]
