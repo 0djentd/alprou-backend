0eab52f1c921e86369fd84b79d3a0a11bd3bb2e6
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-14 18:12:22 +0300

    modified:   reset_db.sh

--- reset_db.sh
+++ reset_db.sh
@@ -2,3 +2,11 @@
 
 fd -t d -HI __pycache__ -X rm -r 
 fd -t d -HI .mypy_cache -X rm -r 
+fd -t d -HI migrations -X rm -r 
+
+mkdir core/migrations
+mkdir api/migrations
+touch core/migrations/__init__.py
+touch api/migrations/__init__.py
+
+rm db.sqlite3
