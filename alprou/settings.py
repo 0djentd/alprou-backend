d29473019a0a704ea897b8c17862c63835eace38
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-16 06:52:38 +0300

    Changed versioning.

--- alprou/settings.py
+++ alprou/settings.py
@@ -138,6 +138,8 @@ CORS_ORIGIN_WHITELIST = (
 
 # DRF
 REST_FRAMEWORK = {
+    'DEFAULT_VERSIONING_CLASS':
+        'rest_framework.versioning.NamespaceVersioning',
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle',
