729190f4552fd412b131fb3d21ebef969147b489
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-16 12:46:21 +0300

    modified:   api/serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -6,7 +6,8 @@ from core.models import Profile, Habit, Day
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Profile
-        exclude = []
+        fields = ["id", "url", "user", "public", "public_username", "profile_image", "background_image"]
+        read_only_fields = ["id", "url", "user"]
 
 
 class HabitSerializer(serializers.HyperlinkedModelSerializer):
@@ -14,16 +15,18 @@ class HabitSerializer(serializers.HyperlinkedModelSerializer):
         model = Habit
         fields = ["id", "url", "name", "description", "user",
                   "active", "negative", "public", "completed"]
+        read_only_fields = ["id", "url", "user", "completed"]
 
 
 class DaySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Day
-        exclude = []
+        fields = ["id", "url", "user", "habit", "datetime", "public"]
+        read_only_fields = ["id", "url", "user", "habit", "datetime"]
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'id', 'habits']
-        read_only_fields = ['habits']
+        fields = ['id', 'url', 'profile', 'habits', 'days']
+        read_only_fields = ["id", "url", 'profile', 'habits', 'days']
