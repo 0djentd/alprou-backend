2f42b7325a872a743faff4f509af13774cd14c60
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-17 00:06:45 +0300

    modified:   docs/urls.py

--- docs/urls.py
+++ docs/urls.py
@@ -1,17 +1,22 @@
-from django.urls import path
+from django.urls import path, re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
 schema_view = get_schema_view(
-        openapi.Info(
-            title="Alprou API",
-            default_version="v1",
-            ),
-        public=False,
-        )
+    openapi.Info(
+        title="Alprou API",
+        default_version="v1",
+        description="Minimalistic habits tracker."
+    ),
+    public=False,
+)
 
 urlpatterns = [
-        path("swagger/", schema_view.with_ui('swagger'), name="schema-swagger"),
-        path("redoc/", schema_view.with_ui('redoc'), name="schema-redoc"),
-        ]
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger',
+            cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc',
+            cache_timeout=0), name='schema-redoc'),
+]
