8ac992bdb64645c27258b45eb031edc77c74a204
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-16 03:36:25 +0300

    modified:   api/views.py

--- api/views.py
+++ api/views.py
@@ -14,12 +14,21 @@ logger.setLevel(logging.DEBUG)
 
 
 class ProfilesViewset(viewsets.ModelViewSet):
-    queryset = Profile.objects.all()
+    model = Profile
     serializer_class = serializers.ProfileSerializer
     permission_classes = [IsObjectPublic, IsObjectAuthorPermission, IsAdminUser]
 
+    def get_queryset(self):
+        queryset_public = self.model.objects.all().filter(public=True)
+        queryset_user = self.model.objects.all().filter(user=self.request.user)
+        return queryset_public | queryset_user
 
 class HabitsViewset(viewsets.ModelViewSet):
-    queryset = Habit.objects.all()
+    model = Habit
     serializer_class = serializers.HabitSerializer
     permission_classes = [IsObjectPublic, IsObjectAuthorPermission, IsAdminUser]
+
+    def get_queryset(self):
+        queryset_public = self.model.objects.all().filter(public=True)
+        queryset_user = self.model.objects.all().filter(user=self.request.user)
+        return queryset_public | queryset_user
