50846ee5b7e035ba8b255ae9a9f3f50af1aca95a
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-20 07:24:23 +0300

    modified:   alprou/settings.py

--- alprou/settings.py
+++ alprou/settings.py
@@ -150,6 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
+    'http://localhost:8080',
 )
 
 # DRF
