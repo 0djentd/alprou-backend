4ac368b855bc899172dcc0bfc623170ea4c1321f
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-26 17:32:47 +0300

    new file:   compile.sh

---
+++ compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd frontend/frontend
+npm install
+npm run build
+cd ../..
+source .venv/bin/activate
+./manage.py collectstatic --no-input
+mkdir -p frontend/templates/frontend
+cp static/index.html frontend/templates/frontend/index.html
+sed -i 's/href="\//href="\/static\//g' frontend/templates/frontend/index.html
+sed -i 's/src="\//src="\/static\//g' frontend/templates/frontend/index.html
+
