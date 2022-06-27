62772a3bf851a2783ecf579e093dc0b3ad946bb9
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-27 11:53:47 +0300

    modified:   frontend/frontend modified:   scripts/compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -6,6 +6,6 @@ source .venv/bin/activate
 ./manage.py collectstatic --no-input
 mkdir -p frontend/templates/frontend
 cp static/alprou-frontend/index.html frontend/templates/frontend/index.html
-sed -i 's/href="\//href="\/static\/alprou-fronten\//g' frontend/templates/frontend/index.html
-sed -i 's/src="\//src="\/static\/alprou-fronten\//g' frontend/templates/frontend/index.html
+sed -i 's/href="\//href="\/static\/alprou-frontend\//g' frontend/templates/frontend/index.html
+sed -i 's/src="\//src="\/static\/alprou-frontend\//g' frontend/templates/frontend/index.html
 
