b54850b9a0a52d10595e6d8a8794c9dc94e7dcc8
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-29 07:55:03 +0300

    modified:   alprou/settings.py

--- alprou/settings.py
+++ alprou/settings.py
@@ -155,7 +155,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = ()
+CORS_ORIGIN_WHITELIST = ("https://0djentd.pythonanywhere.com")
 
 # DRF
 REST_FRAMEWORK = {
